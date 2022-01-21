4549232db83601ad4887a85f6beae94e5e476ab9
Fange <fange173@qq.com>
2022-01-21 19:34:10 +0800

    💕Init.

---
+++ README.md
@@ -0,0 +1,31 @@
+# 电量信息
+
+Quasar v2 + Vue3 + Cordova 开发的电量信息demo，面向移动端
+预览：https://fange173.github.io/quasar-power/dist/spa/#/
+
+# Quasar App (quasar-demo)
+
+A Quasar Framework app
+
+## Install the dependencies
+```bash
+yarn
+```
+
+### Start the app in development mode (hot-code reloading, error reporting, etc.)
+```bash
+quasar dev
+```
+
+### Lint the files
+```bash
+yarn run lint
+```
+
+### Build the app for production
+```bash
+quasar build
+```
+
+### Customize the configuration
+See [Configuring quasar.conf.js](https://v1.quasar.dev/quasar-cli/quasar-conf-js).
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated style="height: 85px;">
+      <q-toolbar >
+        <q-toolbar-title style="margin-top: 40px; margin-left: 8px;">
+          <q-icon name="mood" style="font-size: 1.5rem; margin-bottom: 3px;" /> 电量信息
+        </q-toolbar-title>
+        <div style="margin-top: 40px; margin-right: 8px; font-size: 16px;">星恒 48V 24Ah</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: { },
+
+  setup () {
+    return { }
+  }
+})
+</script>
